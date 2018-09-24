2d902e92989040f4279beafe64016e81e5c3cf3d
Anthony <anthonysokolov20@gmail.com>
2018-09-24 08:33:29 -0400

    Added fizzbuzz

---
+++ lab_02/fizzbuzz.py
@@ -0,0 +1,18 @@
+# Anthony & Will
+
+def fizzBuzz(max_value):
+    i = 1
+    while (i <= max_value):
+        output = ""
+        if i % 3 == 0:
+            output+="Fizz"
+        if i % 5 == 0:
+            output+="Buzz"
+        if output:
+            print(output)
+        else:
+            print(i)
+        i+=1
+    return int(max_value / 15)
+
+fizzBuzz(100)
