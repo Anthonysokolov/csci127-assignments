c9b1d41afa9943204e5efd10bf701852d1fb3618
Anthony <anthonysokolov20@gmail.com>
2018-10-18 08:32:58 -0400

    Completed lab

--- lab_03/madlibs.py
+++ lab_03/madlibs.py
@@ -2,7 +2,9 @@
 
 import random
 
+# Madlib sentence
 sentence =  '<NAME> goes to <PLACE> in order to <VERB> their <NOUN> and eat some <FOOD> at <NUMBER> in the morning.'
+# Options for filling in words
 names = ['John', 'Cena', 'Billy', 'Erica']
 places = ['hell', 'school','New York','subway']
 verbs = ['program','review','watch','enjoy']
@@ -12,14 +14,17 @@ numbers = ['4','3','28','7']
 
 
 fills  = ['<NAME>','<PLACE>','<VERB>','<NOUN>','<FOOD>','<NUMBER>']
+words = [names,places,verbs,nouns,foods,numbers]
 
 def madlib(s):
+    '''
+    Takes in a sentence and replaces designated words marked with '<>'
+    '''
     out = []
     for w in s.split():
-        if w == '<NAME>':
-            out.append(names[random.randint(0,len(names)-1)])
-        elif w == '<PLACE>':
-            out.append(places[random.randint(0,len(places)-1)])
+        if w in fills:
+            ind = fills.index(w)
+            out.append(words[ind][random.randint(0, len(words[ind])-1)])
         else:
             out.append(w)
     return ' '.join(out)
