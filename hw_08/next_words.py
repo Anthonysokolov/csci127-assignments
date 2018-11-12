217f59e4540b942f00660ff432447d201f0bb4d5
Anthony <anthonysokolov20@gmail.com>
2018-11-12 09:21:00 -0500

    Added next_words.py

---
+++ hw_08/next_words.py
@@ -0,0 +1,30 @@
+# Kushendra Ramrup and Anthony Sokolov
+
+def clean_data(s):
+    result=""
+    for letter in s:
+        if letter in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            result = result + letter.lower()
+        else:
+            result = result + " "
+    return result
+
+
+def next_word_dict(s):
+    out = {}
+    s = s.split()
+    
+    for i in range(len(s)-1):
+        out.setdefault(s[i],[])
+        out[s[i]].append(s[i+1])
+    
+    return out
+
+file="moby-small.txt"
+f = open(file)
+s = clean_data(f.read())
+f.close()
+
+next_words = next_word_dict(s)
+print(next_words)
+        
