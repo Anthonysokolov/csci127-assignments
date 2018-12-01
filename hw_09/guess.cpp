65778c78607299b66811f0e0a91830d59218d0f1
Anthony <anthonysokolov20@gmail.com>
2018-12-01 08:57:21 -0500

    Updated guess.cpp

--- hw_09/guess.cpp
+++ hw_09/guess.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <stdlib.h>
 
 int main(){
-  std::cout << "Please enter a number between 0 and 99: ";
   int x;
+  int guess = 50;
+  int high = 100;
+  int low = 0;
+  int max = 100;
+  int res;
+  int range;
+  bool loop = 1;
+
+  std::cout << "Please enter a number between 0 and 99: ";
   std::cin >> x;
-  std::cout << x << std::endl;
+
+  while(loop == 1){
+    guess = rand() % max + low;
+    std::cout << "Is " << guess << " your number?: ";
+    std::cin >> res;
+    if(res == 0){
+      loop = 0;
+      }
+    else if(res == 1){
+      low = guess + 1;
+
+      }
+    else if(res == -1){
+      high = guess;
+      }
+    max = high - low;
+  }  
+  std::cout << "Your number is " << guess << std::endl;
   return 0;
-}
+} 
+
